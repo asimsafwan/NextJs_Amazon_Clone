c39dcf3739ba563a7c8f0849581591dbf6c91f0a
Mohammed Asim Safwan <mohammed.safwan.18.1@mountblue.tech>
2022-04-24 14:49:37 +0530

    listing products on the main page

--- components/Layout.js
+++ components/Layout.js
@@ -3,21 +3,21 @@ import Head from 'next/head';
 import { AppBar, Toolbar, Typography, Container } from '@material-ui/core';
 import useStyles from '../utils/styles';
 
-export default function Layout() {
+export default function Layout({ children }) {
   const classes = useStyles();
   return (
     <div>
       <Head>
         <title>Next Amazona</title>
       </Head>
-      <AppBar position="static" classes={classes.navbar}>
+      <AppBar position="static" className={classes.navbar}>
         <Toolbar>
           <Typography>amazona</Typography>
         </Toolbar>
       </AppBar>
       <Container className={classes.main}>{children}</Container>
       <footer className={classes.footer}>
-        <Typography>All rights reserved</Typography>
+        <Typography>All rights reserved. Next Amazona.</Typography>
       </footer>
     </div>
   );
