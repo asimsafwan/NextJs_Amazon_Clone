43990e3cd85dd01a87361483be341cb284decb9e
Mohammed Asim Safwan <mohammed.safwan.18.1@mountblue.tech>
2022-04-24 17:39:32 +0530

    Successfully built the product page

--- components/Layout.js
+++ components/Layout.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import Head from 'next/head';
+import NextLink from 'next/link';
 import { AppBar, Toolbar, Typography, Container, Link } from '@material-ui/core';
 import useStyles from '../utils/styles';
-import NextLink from 'next/link';
 
-export default function Layout({ children }) {
+export default function Layout({ title, description, children }) {
   const classes = useStyles();
   return (
     <div>
       <Head>
-        <title>Next Amazona</title>
+        <title>{title ? `${title} - Next Amazona` : 'Next Amazona'}</title>
+        {description && <meta name="description" content={description}></meta>}
       </Head>
       <AppBar position="static" className={classes.navbar}>
         <Toolbar>
--- utils/styles.js
+++ utils/styles.js
@@ -1,10 +1,10 @@
 import { makeStyles } from '@material-ui/core';
 
 const useStyles = makeStyles({
-  navabar: {
+  navbar: {
     backgroundColor: '#203040',
     '& a': {
-      color: '#fff',
+      color: '#ffffff',
       marginLeft: 10,
     },
   },
@@ -19,8 +19,12 @@ const useStyles = makeStyles({
     minHeight: '80vh',
   },
   footer: {
+    marginTop: 10,
     textAlign: 'center',
   },
+  section: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
 });
-
 export default useStyles;
